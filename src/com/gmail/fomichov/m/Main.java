36e0830bb691bc5bdbf1dcfd88b55320f126bf44
Maksym <m.fomichov@gmail.com>
2017-07-06 19:32:22 +0300

    сделал

--- src/com/gmail/fomichov/m/Main.java
+++ src/com/gmail/fomichov/m/Main.java
@@ -1,8 +1,33 @@
 package com.gmail.fomichov.m;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int a, b;
+        System.out.println("Введите первое число:");
+        a = checkInt();
+        System.out.println("Введите второе число:");
+        b = checkInt();
+        System.out.println(b + " " + a);
     }
+
+    // проверяем на целое число
+    private static int checkInt() {
+        Scanner sc = new Scanner(System.in);
+        int value;
+        while (true) {
+            try {
+                value = sc.nextInt();
+                sc.nextLine();
+                break;
+            } catch (Exception e) {
+                System.out.println("Только целое число!");
+                sc.nextLine();
+            }
+        }
+        return value;
+    }
+
 }
