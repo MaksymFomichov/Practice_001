a2b40534c43da2357996e6a4924d284c969ee564
Maksym <m.fomichov@gmail.com>
2017-07-06 20:19:00 +0300

    повтор

--- src/com/gmail/fomichov/m/Main_2.java
+++ src/com/gmail/fomichov/m/Main_2.java
@@ -1,7 +1,45 @@
 package com.gmail.fomichov.m;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 /**
  * Created by mfomi on 06.07.2017.
  */
 public class Main_2 {
+    public static void main(String[] args) {
+        System.out.println("Введите размер массива");
+        int size = checkInt();
+        int dataArray[] = new int[size];
+        int dataArrayReverse[] = new int[dataArray.length];
+        for (int i = 0; i < size; i++) {
+            System.out.println("Введите " + (i + 1) + " элемент массива из " + size);
+            dataArray[i] = checkInt();
+        }
+
+        for (int i = 0; i < dataArray.length; i++) {
+            dataArrayReverse[i] = dataArray[dataArray.length - i - 1];
+        }
+
+        System.out.println("Первый массив " + Arrays.toString(dataArray));
+        System.out.println("Перевернутый массив " + Arrays.toString(dataArrayReverse));
+
+    }
+
+    // проверяем на целое число
+    private static int checkInt() {
+        Scanner sc = new Scanner(System.in);
+        int value;
+        while (true) {
+            try {
+                value = sc.nextInt();
+                sc.nextLine();
+                break;
+            } catch (Exception e) {
+                System.out.println("Только целое число!");
+                sc.nextLine();
+            }
+        }
+        return value;
+    }
 }
